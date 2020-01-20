9addc2e9bfae0e3c1bf37e0b9b020f99a39f526d
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-01-20 13:24:28 -0500

    demoEnums

---
+++ 04/demoEnums/demoEnums.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace enumsDemo
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Epress";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
