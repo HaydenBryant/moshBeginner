07a9e3ed75218c73b1e1fb8b30e67465352b1af7
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-02-04 12:33:13 -0500

    string builder

---
+++ 08/stringBuilder.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+
+namespace stringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine(builder[0]);
+        }
+    }
+}
